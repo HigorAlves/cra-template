4c6b6ba548fca34a43b6fdae91ab35581f2bde09
Higor Alves <higorhaalves@gmail.com>
2021-02-15 18:24:58 -0300

    revert: remove sass from the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import ReactDOM from 'react-dom'
-import 'assets/styles/globals.scss'
 
 import Router from './router'
 import * as serviceWorker from './serviceWorker'
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,8 +5,6 @@ import { useTranslation } from 'react-i18next'
 
 import logo from 'assets/svg/logo.svg'
 
-import styles from './Home.module.scss'
-
 export function HomePage(): JSX.Element {
   const { t: languages } = useTranslation('languages', { useSuspense: false })
   const { t, i18n } = useTranslation('home', { useSuspense: false })
@@ -31,16 +29,13 @@ export function HomePage(): JSX.Element {
         <meta property='og:url' content={t('metatags.url')} />
       </Helmet>
 
-      <div className={styles.app}>
-        <header className={styles.header}>
-          <span className={styles.language} onClick={toggleLanguage}>
-            {languages(language)}
-          </span>
+      <div>
+        <header>
+          <span onClick={toggleLanguage}>{languages(language)}</span>
 
-          <img src={logo} className={styles.logo} alt='logo' />
+          <img src={logo} alt='logo' />
           <p>{t('content')}</p>
           <a
-            className={styles.link}
             href='https://reactjs.org'
             target='_blank'
             rel='noopener noreferrer'
